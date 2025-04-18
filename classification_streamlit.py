506b9ddd56dc374440e6f99c3c71cce73d781f38
Gust99-glitch <gustavmensah@icloud.com>
2025-04-17 22:11:37 -0400

    Update classification_streamlit.py

--- classification_streamlit.py
+++ classification_streamlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
 
